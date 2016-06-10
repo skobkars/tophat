274c432cc02750efc4364a48111729c22f9f2916
Sergey Skobkarev <sergsk@gmail.com>
2016-06-09 19:21:13 -0700

    use all_input instead of keyboad and mouse for Kobo

--- src/Event/Poll/InputQueue.hpp
+++ src/Event/Poll/InputQueue.hpp
@@ -29,7 +29,8 @@ Copyright_License {
 #else
 #include "Linux/MergeMouse.hpp"
 #ifdef KOBO
-#include "Linux/Input.hpp"
+//#include "Linux/Input.hpp"
+#include "Linux/AllInput.hpp"
 #elif defined(USE_LINUX_INPUT)
 #include "Linux/AllInput.hpp"
 #else
@@ -53,8 +54,9 @@ class InputEventQueue final {
 #else /* !USE_LIBINPUT */
   MergeMouse merge_mouse;
 #ifdef KOBO
-  LinuxInputDevice keyboard;
-  LinuxInputDevice mouse;
+  /*LinuxInputDevice keyboard;
+  LinuxInputDevice mouse;*/
+  AllLinuxInputDevices all_input;
 #elif defined(USE_LINUX_INPUT)
   AllLinuxInputDevices all_input;
 #else
